609e9e24232dc340cb0e323933b96e64fe9a79ee
Edgar Irizarry <eirizarry15@yahoo.com>
2018-09-11 18:22:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array,element){
-  
+  addElementToBeginningOfArray.unshift("food")
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
