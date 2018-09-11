5e8521423f29719c8ef6eaa700ed200f3a2cb441
Edgar Irizarry <eirizarry15@yahoo.com>
2018-09-11 18:06:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array,element){
-  ["foo",... chocolateBars]
+  ["foo",1, ... chocolateBars]
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
